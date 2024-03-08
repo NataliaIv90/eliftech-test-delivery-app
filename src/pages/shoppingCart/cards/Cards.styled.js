34d074c10e1e1f5b40a69edc3e2661c019a5f531
Natalia Ivantsova <ivantsovana@gmail.com>
2024-03-08 10:46:42 +0200

    fix: fix the height in the cart

--- src/pages/shoppingCart/cards/Cards.styled.js
+++ src/pages/shoppingCart/cards/Cards.styled.js
@@ -3,4 +3,7 @@ import { styled } from "@mui/material";
 export const CardsWrapper = styled('div')({
   gridArea: 'cards',
   width: '100%',
+  maxHeight: '70vh',
+  overflowY: 'scroll',
+  padding: '15px',
 })
